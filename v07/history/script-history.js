83dee148abc678bbd674e525a3bc9920f28f459a
eth-infinitism <dror.tirosh@gmail.com>
2024-02-22 18:27:55 +0000

    Update from https://github.com/eth-infinitism/bundler-test-executor/commit/a8296589daa05cb8d5be1e432f452f6443c93b71

--- v07/history/script-history.js
+++ v07/history/script-history.js
@@ -1,1 +1,31 @@
-testHistory={}
+testHistory={
+  "20240222/182751": {
+    "aabundler": {
+      "name": "aabundler",
+      "errors": "0",
+      "failures": "0",
+      "skipped": "0",
+      "tests": "3",
+      "time": "1.098",
+      "timestamp": "2024-02-22T18:27:45.782457",
+      "hostname": "fv-az1020-984",
+      "testcase": {
+        "0": {
+          "classname": "tests.single.opbanning.test_op_banning",
+          "name": "test_account_banned_opcode[GASPRICE]",
+          "time": "0.254"
+        },
+        "1": {
+          "classname": "tests.single.opbanning.test_op_banning",
+          "name": "test_paymaster_banned_opcode[GASPRICE]",
+          "time": "0.348"
+        },
+        "2": {
+          "classname": "tests.single.opbanning.test_op_banning",
+          "name": "test_factory_banned_opcode[GASPRICE]",
+          "time": "0.418"
+        }
+      }
+    }
+  }
+}
